614fee499ae78dcd94933e201c03c7ccd6afea7e
mariamothman-ui <mariamothman6002@gmail.com>
2026-02-02 01:07:05 +0200

    Create problem c.cpp

---
+++ sheet 1/problem c.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    // long long to avoid over flow 
+    long long x ; 
+    long long y ; 
+    cin >> x >> y ;
+    cout << x <<" "<<"+"<<" "<<y<<" "<<"="<<" "<<x+y<< endl ;
+    cout << x <<" "<<"*"<<" "<<y<<" "<<"="<<" "<<x*y<< endl ;
+    cout << x <<" "<<"-"<<" "<<y<<" "<<"="<<" "<<x-y<< endl ;
+}

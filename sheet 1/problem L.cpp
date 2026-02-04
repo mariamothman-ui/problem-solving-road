08d2b53b8453e505bf48db90388f5f7f9ce25bff
mariamothman-ui <mariamothman6002@gmail.com>
2026-02-04 21:23:04 +0200

    Create problem L.cpp

---
+++ sheet 1/problem L.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    string f_n_1 ;
+    string s_n_1;
+    string f_n_2;
+    string s_n_2;
+    cin >> f_n_1 >> s_n_1 >> f_n_2 >> s_n_2;
+    if (s_n_1==s_n_2){
+        cout <<"ARE Brothers";
+    }
+    else {
+        cout << "NOT";
+    }
+    
+}
+    

fcdac2bbb0943382dbf4f1f2cc3d9aeb563c2ab7
mariamothman-ui <mariamothman6002@gmail.com>
2026-02-04 20:59:57 +0200

    Create problem I.cpp

---
+++ sheet 1/problem I.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    double x , y ;
+    cin >> x >> y ;
+    if (x >= y){
+        cout << "Yes";
+    }
+    else {
+        cout <<"No";
+    }
+}
+    

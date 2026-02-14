192b982d71548b90cf4ad6b2621b146453c42c9c
mariamothman-ui <mariamothman6002@gmail.com>
2026-02-14 22:52:05 +0200

    Create problem E.cpp

---
+++ sheet 2 (loops)/problem E.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+using namespace std ;
+int main()
+{
+    int n ;
+    cin >> n ;
+    int max = INT_MIN;
+    for (int i = 0 ; i < n ; i++){
+        int x ;
+        cin >> x ;
+        if (x > max ){
+            max = x ;
+        }
+    }
+    cout << max ;
+}
